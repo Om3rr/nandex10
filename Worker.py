8d0dc49f6fe3592814f8f19ec57409777dd26147
Omer <Omertorrents@gmail.com>
2017-01-05 23:50:56 +0200

    aluf

--- Worker.py
+++ Worker.py
@@ -89,15 +89,20 @@ class Worker:
         pass
 
     def compile_subroutine_call(self, indentation=0):
-        pass
+        keyword = self.tokens[-1]
+        if(keyword[0] == 'let'):
+            self.compile_let(indentation)
+
+    def compile_let(self, indentation=0):
+
 
     def compile_expression_list(self, keyword, indentation=0):
-        self.compile_symbol(keyword,indentation)
-        keyword = self.tokens.pop()[0]
-        while(keyword != ','):
-            self.compile_expression(keyword, indentation)
-            keyword = self.tokens.pop()[0]
-        self.compile_symbol(keyword, indentation)
+        self.compile_symbol(indentation) ## (
+        keyword = self.tokens[-1]
+        while(keyword[0] != ')'):
+            self.compile_expression(indentation)
+            keyword = self.tokens[-1]
+            self.compile_symbol(indentation)
 
     def compile_op(self, indentation=0):
         self.compile_term(indentation)
@@ -111,8 +116,8 @@ class Worker:
     def compile_keyword_constant(self, indentation=0):
         keyword = self.tokens.pop()
         if (self.dbg):
-            if (keyword[1] != 'symbol'):
-                print(str(keyword) + " != symbol")
+            if (keyword[1] != 'keyword'):
+                print(str(keyword) + " != keyword")
         self.__file.write('\t' * indentation + '<keyword> ' + keyword[0] + ' </keyword>\n')
 
     def compile_symbol(self, indentation=0):
@@ -128,6 +133,15 @@ class Worker:
             if(keyword[1] != 'identifier'):
                 print(str(keyword)+" != identifier")
         self.__file.write('\t' * indentation + '<identifier> ' + keyword[0] + ' </identifier>\n')
+
+    def writeLine(self, tag, keyword, indentation):
+        return '%s<%s> %s </%s>'%('\t'*indentation, tag, keyword[0], tag)
+    
+    def writeSingle(self, tag, indent, toOpen=True):
+        if(toOpen):
+            return '%s<%s>' %('\t' * indent, tag)
+        else:
+            return '%s</%s>' %('\t' * indent, tag)
 
 
 a = iter([('class', 'class'), ('Main', 'identifier'), ('{', 'symbol'), ('static', 'classVarDec'), ('int', 'type'),
